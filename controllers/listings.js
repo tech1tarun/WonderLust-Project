996b345b8c71508ea77115192317f2fceb148432
tech1tarun <tarunkumarsingh248@gmail.com>
2026-01-16 21:13:33 +0530

    image fixing and cloudinary multer

--- controllers/listings.js
+++ controllers/listings.js
@@ -22,10 +22,13 @@ module.exports.showListing = async (req, res) => {
 };
 
 module.exports.createListing = async (req, res, next) => {
+  let url = req.file.path;
+  let filename = req.file.filename;
   //passing an instance on listing
   //try beacuse of error handling then try is replace by warapAsync function
   const newListing = new Listing(req.body.listing);
   newListing.owner = req.user._id;
+  newListing.image = { url, filename };
   await newListing.save();
   req.flash("success", "New Listing Created");
   res.redirect("/listings");
@@ -38,12 +41,22 @@ module.exports.editListing = async (req, res) => {
     req.flash("error", "Listing you requested for does not exist");
     return res.redirect("/listings");
   }
-  res.render("listings/edit.ejs", { listing });
+
+  let originalImageUrl = listing.image.url;
+  originalImageUrl = originalImageUrl.replace("/upload", "/upload/h_300,w_250");
+  res.render("listings/edit.ejs", { listing, originalImageUrl });
 };
 
 module.exports.updateListing = async (req, res) => {
   let { id } = req.params;
-  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  const listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+
+  if (typeof req.file !== "undefined") {
+    let url = req.file.path;
+    let filename = req.file.filename;
+    listing.image = { url, filename };
+    await listing.save();
+  }
   req.flash("success", "Listing Updated");
   res.redirect(`/listings/${id}`);
 };
--- routes/listing.js
+++ routes/listing.js
@@ -1,15 +1,26 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const router = express.Router();
 const wrapAsyn = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
-
 const listingController = require("../controllers/listings.js");
+const multer = require("multer");
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
 
 router
   .route("/")
   .get(wrapAsyn(listingController.index)) //Index Route
-  .post(isLoggedIn, validateListing, wrapAsyn(listingController.createListing)); //Create Route
+  .post(
+    isLoggedIn,
+    validateListing,
+    upload.single("listing[image]"),
+    wrapAsyn(listingController.createListing)
+  ); //Create Route
 
 //New route
 //It must be placed after the route with :id to avoid conflicts (before it would treat "new" as an id)
@@ -21,6 +32,7 @@ router
   .put(
     isLoggedIn,
     isOwner,
+    upload.single("listing[image]"),
     validateListing,
     wrapAsyn(listingController.updateListing) //Update Route
   )
